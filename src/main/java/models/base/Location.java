d22245f211278cb6ce1d0dda8e1792228dae782b
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-09-03 15:35:04 +0200

    Fix ctor

--- src/main/java/models/base/Location.java
+++ src/main/java/models/base/Location.java
@@ -6,6 +6,8 @@ public class Location implements LocationMessage {
     public String id;
     public String name;
 
+    public Location() {}
+
     public Location(String id, String name) {
         this.id = id;
         this.name = name;
